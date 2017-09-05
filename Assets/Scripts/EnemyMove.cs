8416b0fd0795b69517a2505041e94dae5f8362e7
Finn Pinkenburg <finnpinkenburg@googlemail.com>
2017-09-05 14:42:23 +0200

    Navmesh Setup / Enemy Animations Setup

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Assertions;
+
+public class EnemyMove : MonoBehaviour {
+
+    [SerializeField] Transform player;
+    private NavMeshAgent nav;
+    private Animator anim;
+
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+	}
+
+    private void Awake()
+    {
+        Assert.IsNotNull(player);
+    }
+
+    void Update ()
+    {
+        nav.SetDestination(player.position);
+	}
+}

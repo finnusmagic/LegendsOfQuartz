62974a8da38f616b2f0d40d65f32622e9e658434
Finn Pinkenburg <finnpinkenburg@googlemail.com>
2017-09-05 15:14:23 +0200

    Enemy Attacking

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+    [SerializeField] float range = 3f;
+    [SerializeField] float timeBetweenAttacks = 1f;
+
+    private Animator anim;
+    private GameObject player;
+    private bool playerInRange;
+    private BoxCollider[] weaponColliders;
+
+    void Start()
+    {
+        weaponColliders = GetComponentsInChildren<BoxCollider>();
+        player = GameManager.instance.Player;
+        anim = GetComponent<Animator>();
+        StartCoroutine(Attack());
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < range)
+        {
+            playerInRange = true;
+        }
+        else {
+            playerInRange = false;
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        if (playerInRange && !GameManager.instance.GameOver)
+        {
+            anim.Play("Attack");
+            yield return new WaitForSeconds(timeBetweenAttacks);
+        }
+        yield return null;
+        StartCoroutine(Attack());
+    }
+}
